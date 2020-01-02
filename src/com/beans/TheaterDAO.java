e4c036b75fd9c6dd636d8e1105d11baaa6894a28
user <user@DESKTOP-HAGABK2>
2020-01-03 01:05:21 +0900

    일단 DAO 파일만 추가입니다.

---
+++ src/com/beans/TheaterDAO.java
@@ -0,0 +1,44 @@
+package com.beans;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import common.D;
+	
+public class TheaterDAO {
+	Connection conn;
+	PreparedStatement pstmt;
+	Statement stmt;
+	ResultSet rs;
+	
+	// DAO 객체가 생성될때 Connection도 생성된다.
+	public TheaterDAO() {
+		try {
+			Class.forName(D.DRIVER);
+			conn = DriverManager.getConnection(D.URL, D.USERID, D.USERPW);
+			System.out.println("WriteDAO 객체 생성, 데이터베이스 연결");
+			
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	//DB 자원반납 메소드
+	public void close() throws SQLException {
+		if(rs != null) rs.close();
+		if(pstmt != null) pstmt.close();
+		if(stmt != null) stmt.close();
+		if(conn != null) conn.close();
+	}
+}
+
+
