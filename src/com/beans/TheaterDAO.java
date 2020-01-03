dcdee9a47e96c168749de16e20b3447c1db91f89
user <user@DESKTOP-HAGABK2>
2020-01-03 15:04:28 +0900

    20200103- 3:00

--- src/com/beans/TheaterDAO.java
+++ src/com/beans/TheaterDAO.java
@@ -23,7 +23,7 @@ public class TheaterDAO {
 		try {
 			Class.forName(D.DRIVER);
 			conn = DriverManager.getConnection(D.URL, D.USERID, D.USERPW);
-			System.out.println("WriteDAO 객체 생성, 데이터베이스 연결");
+			System.out.println("TheaterDAO 객체 생성, 데이터베이스 연결");
 			
 			
 		} catch (ClassNotFoundException e) {
