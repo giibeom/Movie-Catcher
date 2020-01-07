7d4fc47a01e76a1e15d8fe8f16be780800c0b16f
user <user@DESKTOP-HAGABK2>
2020-01-07 16:09:25 +0900

    DAO 수정

--- src/com/beans/ReserveDAO.java
+++ src/com/beans/ReserveDAO.java
@@ -1,6 +1,140 @@
 package com.beans;
 
-public class ReserveDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
+import common.D;
+
+public class ReserveDAO {
+	Connection conn;
+	PreparedStatement pstmt;
+	Statement stmt;
+	ResultSet rs;
 	
+	public ReserveDAO() {
+		try {
+			Class.forName(D.DRIVER);
+			conn = DriverManager.getConnection(D.URL, D.USERID, D.USERPW);
+			System.out.println("ReserveDAO 객체 생성,데이터 베이스 연결");
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// DB 자원 반납 메소드 
+	public void close() throws SQLException {
+		if(rs != null) rs.close();
+		if(pstmt != null) pstmt.close();
+		if(stmt != null) stmt.close();
+		if(conn != null) conn.close();
+	}
+	
+	public int insert(ReserveDTO dto) throws SQLException {
+		String rs_date = dto.getRs_date();
+		String rs_price = dto.getRs_price();
+		String rs_seat = dto.getRs_seat();
+		int u_idnum = dto.getU_idnum();
+		int t_uid = dto.getT_uid();
+		return this.insert(rs_date, rs_price, rs_seat, u_idnum, t_uid);
+	}
+	
+	public int insert(String rs_date, String rs_price, String rs_seat, int u_idnum, int t_uid) throws SQLException{
+		int cnt = 0;
+		
+		try {
+			pstmt = conn.prepareStatement(D.SQL_RESERVE_INSERT);
+			pstmt.setString(1, rs_date);
+			pstmt.setString(2, rs_price);
+			pstmt.setString(3, rs_seat);
+			pstmt.setInt(4, u_idnum);
+			pstmt.setInt(5, t_uid);
+			cnt = pstmt.executeUpdate();
+		} finally {
+			close();
+		}
+		return cnt;
+	}
+	
+	public ReserveDTO [] createArray(ResultSet rs) throws SQLException {
+		ArrayList<ReserveDTO> list = new ArrayList<ReserveDTO>();
+		
+		while(rs.next()) {
+			
+			int rs_num = rs.getInt("rs_num");
+			String rs_date = rs.getString("rs_date");
+			String rs_price = rs.getString("rs_price");
+			String rs_seat = rs.getString("rs_seat");
+			int u_idnum = rs.getInt("u_idnum");
+			int t_uid = rs.getInt("t_uid");
+			
+			
+			ReserveDTO dto = new ReserveDTO(rs_num, rs_date, rs_price, rs_seat, u_idnum, t_uid);
+			list.add(dto);
+		}
+		int size = list.size();
+		ReserveDTO [] arr = new ReserveDTO[size];
+		list.toArray(arr);
+		return arr;
+		
+	}
+	
+	public ReserveDTO [] getUserRs(int u_idnum) throws SQLException {
+		ReserveDTO [] arr = null;
+		
+		try {
+			pstmt = conn.prepareStatement(D.SQL_RESERVE_USER);
+			pstmt.setInt(1, u_idnum);
+			rs = pstmt.executeQuery();
+			arr = createArray(rs);
+			
+		}finally {
+			close();
+		}
+		
+		return arr;
+		
+	}
+	
+	public ReserveDTO [] get(int rs_num) throws SQLException {
+		ReserveDTO [] arr = null;
+		
+		try {
+			pstmt = conn.prepareStatement(D.SQL_RESERVE_REVIEW);
+			pstmt.setInt(1, rs_num);
+			rs = pstmt.executeQuery();
+			arr = createArray(rs);
+			
+		}finally {
+			close();
+		}
+		
+		return arr;
+		
+	}
+	
+	public int delete(int rs_num) throws SQLException {
+		int cnt = 0;
+		
+		try {
+			pstmt = conn.prepareStatement(D.SQL_RESERVE_DELETE);
+			pstmt.setInt(1, rs_num);
+			cnt = pstmt.executeUpdate();
+			
+		}finally {
+			close();
+		}
+		
+		return cnt;
+		
+	}
 }
